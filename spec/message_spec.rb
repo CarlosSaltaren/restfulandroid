583d2282501e0f108aa4d4be51c694cca48079f9
Carlos Saltaren <andresvalar@hotmail.com>
2014-09-05 11:27:32 +1000

    commit for testing

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -3,19 +3,37 @@ require_relative '../lib/message'
 
 describe MessagePost do
 
+  subject {  MessagePost.new 'author','mid','heading','message','postdate'}
+
   before :each do
-    @messageObj = MessagePost.new 'author','mid','heading','message','postdate'
-  end
+    end
 
 
   describe '#new' do
     it 'should return an object of message' do
-      expect(@messageObj).to be_a MessagePost
+      expect(subject).to be_a MessagePost
     end
-#    it 'should throw an expection error' do
- #    expect(lambda {MessagePost.new 'author','mid','heading'}).
+ #   it 'should throw an expection error' do
+  #   expect(lambda {MessagePost.new 'author','mid','heading'}).
   #   expect(subject.isFileExist fileName ).to eq true
    #end
   end
+
+  describe '#new' do
+    it 'Is any author in the message' do
+      expect(subject).to be_a MessagePost
+    end
+#    it 'should throw an expection error' do
+#    expect(lambda {MessagePost.new 'author','mid','heading'}).
+#   expect(subject.isFileExist fileName ).to eq true
+#end
+  end
+
+
+
+
+
+
+
 
 end
