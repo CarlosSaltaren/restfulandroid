7db8ed4a54fa6b363bec68f5bb659e65a8411a63
martinmutimba <martin.muti@yahoo.com>
2014-09-05 11:12:05 +1000

    Commit 3

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -12,10 +12,9 @@ describe MessagePost do
     it 'should return an object of message' do
       expect(@messageObj).to be_a MessagePost
     end
-
 #    it 'should throw an expection error' do
- #     expect(lambda {MessagePost.new 'author','mid','heading'}).
- #     expect(subject.isFileExist fileName ).to eq true
- #   end
+ #    expect(lambda {MessagePost.new 'author','mid','heading'}).
+  #   expect(subject.isFileExist fileName ).to eq true
+   #end
   end
 end
