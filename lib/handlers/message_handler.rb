3d23eb9750d56340bb5312d8f3dcd7801f28bf7e
Carlos Saltaren <andresvalar@hotmail.com>
2014-09-12 10:53:01 +1000

    Travis exception

--- lib/handlers/message_handler.rb
+++ lib/handlers/message_handler.rb
@@ -3,7 +3,7 @@ class MessageHandler
 
   def get_message
     if(MessageRepository.message==nil || MessageRepository.message=='' )
-      return MessageRepository.message='Have a nice  day'
+      return MessageRepository.message='Have a nice day'
     end
     MessageRepository.message
   end
