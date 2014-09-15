65b20cc43e06cc89bd9f629064cdc4320f3a3979
Carlos Saltaren <andresvalar@hotmail.com>
2014-09-15 10:00:04 +1000

    Test cases

--- lib/handlers/message_handler.rb
+++ lib/handlers/message_handler.rb
@@ -2,10 +2,7 @@ require_relative '../../lib/services/message__repository'
 class MessageHandler
 
   def get_message
-    if(MessageRepository.message==nil || MessageRepository.message=='' )
-      return MessageRepository.message='Have a nice day'
-    end
-    MessageRepository.message
+    MessageRepository.message.nil? ? 'Have a nice day' : MessageRepository.message
   end
 
 
